a5f5ccd192cca2193b1a9397752daaa0097d829e
Jose Ignacio Santa Cruz G <jsanta@random.cl>
2018-09-06 12:45:57 -0300

    CHG Se modifica esquema de rutas para layout con menu lateral

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
+import { PosPage } from './pages/pos/pos.page';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
-  { path: 'pos', loadChildren: './pages/pos/pos.module#PosPageModule' }
+  {
+    path: 'pos',
+    component: PosPage,
+    canActivate: [AuthGuardService],
+    children: [
+      {
+        path: '',
+        loadChildren: './pages/pos/pos.module#PosPageModule',
+      }
+    ]
+  }
 ];
 
 @NgModule({
