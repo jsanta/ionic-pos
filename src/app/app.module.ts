a1750b33623d2cf961da10accb32ba305bf640ee
Jose Ignacio Santa Cruz G <jsanta@random.cl>
2018-09-06 16:23:47 -0300

    ADD Se configura importacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,40 @@
-import { NgModule } from '@angular/core';
+import { ClientListModule } from './components/client-list/client-list.module';
+import { HeaderModule } from './components/header/header.module';
+import { ComponentsModule } from './components/components.module';
+import { PosPage } from './pages/pos/pos.page';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, Injector } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, RouteReuseStrategy, Routes } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { IonicStorageModule } from '@ionic/storage';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ProductListComponent } from './components/product-list/product-list.component';
-import { ProductItemComponent } from './components/product-item/product-item.component';
-import { ClientItemComponent } from './components/client-item/client-item.component';
-import { ClientListComponent } from './components/client-list/client-list.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, ProductItemComponent, ClientItemComponent, ClientListComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+      AppComponent
+      , PosPage
+    ],
+    entryComponents: [],
+    imports: [
+      BrowserModule,
+      IonicModule.forRoot(),
+      AppRoutingModule,
+      IonicStorageModule.forRoot(),
+      ComponentsModule
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {
+            provide: RouteReuseStrategy,
+            useClass: IonicRouteStrategy
+        }
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/components/components.module.ts
@@ -0,0 +1,35 @@
+import { HeaderModule } from './header/header.module';
+import { ClientListModule } from './client-list/client-list.module';
+import { HeaderComponent } from './header/header.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ClientListComponent } from './client-list/client-list.component';
+import { ClientItemComponent } from './client-item/client-item.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule
+  ],
+  declarations: [
+    ClientItemComponent,
+    ClientListComponent,
+    ProductItemComponent,
+    ProductListComponent,
+    HeaderComponent
+  ],
+  exports: [
+    ClientItemComponent,
+    ClientListComponent,
+    ProductItemComponent,
+    ProductListComponent,
+    HeaderComponent
+  ]
+
+})
+export class ComponentsModule { }
